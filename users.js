90bc9a7da24c8a1f2054fd1646a06982de1d7117
stas-lyu <staslyu1997@gmail.com>
2021-06-08 19:29:49 +0300

    add

---
+++ users.js
@@ -0,0 +1,24 @@
+let users = [
+    {
+        id: 1,
+        fullName: 'Ivan Ivanov',
+        phone: 09723123132
+    },
+    {
+        id: 2,
+        fullName: 'Olegov Andrey',
+        phone: 0998923132
+    },
+    {
+        id: 3,
+        fullName: 'Alina Alina',
+        phone: 09723123132
+    },
+    {
+        id: 4,
+        fullName: 'Kaktus Sasha',
+        phone: 09754354332
+    }
+]
+
+let container = document.querySelector('.users_container');
